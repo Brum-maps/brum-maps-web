1ae13d2c66e10e2f502112872f2cf214246d034c
Ad <adrienroux94@gmail.com>
2022-03-10 16:13:33 +0100

    HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <HeaderComponent></HeaderComponent>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <footer-section></footer-section>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+import FooterSection from "@/components/FooterSection";
+import HeaderComponent from "@/components/HeaderComponent";
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  components: {
+    FooterSection,
+    HeaderComponent,
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-footer style="background-color: #AE9387" padless>
+    <v-row justify="center" no-gutters="true">
+      <router-link v-for="link in links" :to="link.link" :key="link" style="text-decoration: none; color: inherit;">
+        <v-btn elevation="0" color="#AE9387" plain text class="py-2 px-16" text-color="white">
+          {{ link.name }}
+        </v-btn>
+      </router-link>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data: () => ({
+    links: [
+      {name: 'Home', link: '/visites'},
+      {name: 'About Us', link: '/'},
+      {name: 'Team', link: '/'},
+      {name: 'Services', link: '/'},
+      {name: 'Blog', link: '/'},
+      {name: 'Contact Us', link: '/'},
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+</style>
